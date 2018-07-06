e65689a9b02e7705f52b89a63a77de505b6716ed
Nikita Isakov <38158635+NikitaIsakov@users.noreply.github.com>
2018-07-06 13:14:59 +0800

    Add files via upload

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,52 @@
-$(document).ready(function(){var t=document.getElementsByClassName("btn-burger-mobile"),n=(document.getElementsByClassName("burger-notification"),document.getElementsByClassName("wrap-nav-mobile")),o=document.getElementsByClassName("btn-burger");t[0].onclick=function(e){o[0].classList.toggle("burger-active"),t[0].classList.toggle("burger-notification"),n[0].classList.toggle("wrap-nav-mobile_active")};var e=$(".owl-carousel");e.owlCarousel({loop:!0,items:1,margin:10,onInitialized:function(e){$(".counter").text("< 1 / "+this.items().length+" > ")}}),e.on("changed.owl.carousel",function(e){$(".counter").text(" < "+ ++e.page.index+" / "+e.item.count+" >")})});
+   $(document).ready(function(){
+
+			var btn = document.getElementsByClassName('btn-burger-mobile');
+				var notification = document.getElementsByClassName('burger-notification');
+				var mobile_nav = document.getElementsByClassName('wrap-nav-mobile');
+				var btn_burger = document.getElementsByClassName('btn-burger');
+
+				btn[0].onclick = function (e) {
+					btn_burger[0].classList.toggle("burger-active");
+					btn[0].classList.toggle("burger-notification");
+					mobile_nav[0].classList.toggle("wrap-nav-mobile_active");
+				};
+
+
+
+
+   	var owl = $('.owl-carousel-1');
+      owl.owlCarousel({
+        loop: true,
+        items: 1,
+        margin: 10,
+        // nav: true,
+        
+        onInitialized: function(e) {
+          $('.ctr-seasons').text('< 1 / ' + this.items().length + ' > ')
+        }
+        
+      });
+      owl.on('changed.owl.carousel', function(e) {
+        $('.ctr-seasons').text(' < ' + ++e.page.index + ' / ' + e.item.count + ' >')
+      }); 
+
+      	var owl1 = $('.owl-carousel-2');
+		      owl1.owlCarousel({
+		        loop: true,
+		        items: 1,
+		        margin: 10,
+		        // nav: true,
+		        
+		        onInitialized: function(e) {
+		          $('.ctr-news').text('< 1 / ' + this.items().length + ' > ')
+		        }
+		        
+		      });
+		      owl1.on('changed.owl.carousel', function(e) {
+		        $('.ctr-news').text(' < ' + ++e.page.index + ' / ' + e.item.count + ' >')
+		      }); 
+
+
+     
+
+    });
